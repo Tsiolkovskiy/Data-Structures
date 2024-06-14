988e227d19e8e56908fc1e150bacd8027b531d16
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-06-14 09:27:18 -0400

    object destructuring

--- script.js
+++ script.js
@@ -30,30 +30,38 @@ const restaurant = {
       close: 24,
     },
   },
+  orderDelivery: function({starterIndex,mainIndex,time,address}){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  }
 };
 
-const arr=[2,3,4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
-
-const[x,y,z] =arr;
-console.log(x,y,z);
-
-let [main, ,secondary]=restaurant.categories;
-console.log(main,secondary);
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole,21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
 
-[main,secondary]=[secondary,main];
-console.log(main,secondary);
 
-const [starter, mainCourse] = restaurant.order(2,0);
-console.log(starter,mainCourse);
 
-const nested = [2,4,[5,6]];
-const [i, , [j,k]] = nested;
-console.log(i,j,k);
+const{name,openingHours,categories}=restaurant;
+console.log(name, openingHours,categories);
 
-const[p=1, q=1,r=1]=[8,9];
-console.log(p,q,r);
+const{name:restaurantName, openingHours:hours, categories:tags}=restaurant;
+console.log(restaurantName, hours, tags);
 
+const{menu=[], starterMenu: starters=[]}=restaurant;
+console.log(menu,starters);
+
+let a=111;
+let b = 999;
+const obj={a:23, b:7, c:14};
+({a,b}=obj);
+console.log(a,b);
+
+const {
+  fri:{
+    open:o,close:c},
+}=openingHours;
+console.log(o,c);
