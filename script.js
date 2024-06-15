6d516f06f976caed3a70f76c82c8e850c45f674a
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-06-15 17:34:08 -0400

    short circuiting js

--- script.js
+++ script.js
@@ -45,35 +45,59 @@ const restaurant = {
   }
 };
 
-const arr=[1,2,...[3,4]];
-
-const [a,b, ...others]=[1,2,3,4,5];
-
-console.log(a,b,others);
 
 
-const [pizza, ,risotto, ...otherFood]=[...restaurant.mainMenu,...restaurant.starterMenu];
 
-console.log(pizza,risotto,otherFood);
-//objects
-const {sat,...weekdays}= restaurant.openingHours;
-console.log(weekdays);
-//functions
-const add = function(...numbers){
-  let sum=0;
-  for(let i=0;i<numbers.length;i++) sum+=numbers[i];
-  console.log(sum)
+console.log(3||'Jonas');
+console.log(''||'Jonas');
+console.log(true||'Jonas');
+console.log(undefined||null);
+console.log(undefined||0||''||'Hello'||23||null);
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 =restaurant.numGuests || 10 
+console.log(guests2);
+
+console.log(0&&'Jonas');
+console.log(7&&'Jonas');
+
+if (restaurant.orderPizza){
+    restaurant.orderPizza('mushrooms', 'spincach');
 }
 
-add(2,3);
-add(5,3,7,2);
-add(8,2,5,3,2,1,4);
+restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
 
-const x=[23,5,7];
-add(...x);
+// const arr=[1,2,...[3,4]];
 
-restaurant.orderPizza('mushrooms','onions','olives','spinach');
-restaurant.orderPizza('mushrooms');
+// const [a,b, ...others]=[1,2,3,4,5];
+
+// console.log(a,b,others);
+
+
+// const [pizza, ,risotto, ...otherFood]=[...restaurant.mainMenu,...restaurant.starterMenu];
+
+// console.log(pizza,risotto,otherFood);
+// //objects
+// const {sat,...weekdays}= restaurant.openingHours;
+// console.log(weekdays);
+// //functions
+// const add = function(...numbers){
+//   let sum=0;
+//   for(let i=0;i<numbers.length;i++) sum+=numbers[i];
+//   console.log(sum)
+// }
+
+// add(2,3);
+// add(5,3,7,2);
+// add(8,2,5,3,2,1,4);
+
+// const x=[23,5,7];
+// add(...x);
+
+// restaurant.orderPizza('mushrooms','onions','olives','spinach');
+// restaurant.orderPizza('mushrooms');
 
 
 // const ingredients = [prompt('Let\'s make pasta! Ingredient 1?' ), prompt('Ingredient 2'), prompt("Ingredient 3"),];
