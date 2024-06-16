e1328f003c7ba9cf8d867e5d498ae4cdd18068c4
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-06-16 09:43:44 -0400

    Nullish coalescing operator

--- script.js
+++ script.js
@@ -46,28 +46,34 @@ const restaurant = {
 };
 
 
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
 
 
-console.log(3||'Jonas');
-console.log(''||'Jonas');
-console.log(true||'Jonas');
-console.log(undefined||null);
-console.log(undefined||0||''||'Hello'||23||null);
+// console.log(3||'Jonas');
+// console.log(''||'Jonas');
+// console.log(true||'Jonas');
+// console.log(undefined||null);
+// console.log(undefined||0||''||'Hello'||23||null);
 
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
 
-const guests2 =restaurant.numGuests || 10 
-console.log(guests2);
+// const guests2 =restaurant.numGuests || 10 
+// console.log(guests2);
 
-console.log(0&&'Jonas');
-console.log(7&&'Jonas');
+// console.log(0&&'Jonas');
+// console.log(7&&'Jonas');
 
-if (restaurant.orderPizza){
-    restaurant.orderPizza('mushrooms', 'spincach');
-}
+// if (restaurant.orderPizza){
+//     restaurant.orderPizza('mushrooms', 'spincach');
+// }
 
-restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
+// restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
 
 // const arr=[1,2,...[3,4]];
 
