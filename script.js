240ba9895a99f3424aaac870a8cf8451ac38e30c
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-06-18 07:16:50 -0400

    optional Chaining

--- script.js
+++ script.js
@@ -100,20 +100,20 @@ const restaurant = {
 
   hours,
 
-  // openingHours: {
-  //   thu: {
-  //     open: 12,
-  //     close: 22,
-  //   },
-  //   fri: {
-  //     open: 11,
-  //     close: 23,
-  //   },
-  //   sat: {
-  //     open: 0, // Open 24 hours
-  //     close: 24,
-  //   },
-  // },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
   orderDelivery({starterIndex,mainIndex,time,address}){
     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
@@ -129,9 +129,21 @@ const restaurant = {
   }
 };
 
+// if (restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
 
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+const days = ['mon','tue', 'wed','thu', 'fri','sat', 'sun'];
+for(const day of days){
+  console.log(day);
+  const open=restaurant.openingHours[day]?.open ??'closed';
+  console.log(`On ${day} we open at ${open}`);
+}
+console.log(restaurant.order?.(0,1)??'Method does not exist');
+console.log(restaurant.orderRisotto?.(0.1)??'Method does not exist');
 
-
+const users = [{name: 'Jonas',emaile: 'hello@jonas.io'}];
+console.log(users[0]?.name ?? 'User array empty');
 
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
