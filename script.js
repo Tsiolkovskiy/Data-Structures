223f2d71eaf08183250524c72238a0caca406492
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-06-17 22:04:16 -0400

    Enhanced Object Literals

--- script.js
+++ script.js
@@ -65,6 +65,23 @@
 
 'use strict';
 
+const weekdays = ['mon','tue', 'wed','thu', 'fri','sat', 'sun'];
+
+const hours= {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -77,29 +94,31 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order:function(starterIndex,mainIndex){
+  order(starterIndex,mainIndex){
       return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  orderDelivery: function({starterIndex,mainIndex,time,address}){
+  hours,
+
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
+  orderDelivery({starterIndex,mainIndex,time,address}){
     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
 
-  orderPasta: function(ing1, ing2, ing3){
+  orderPasta(ing1, ing2, ing3){
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
 
   },
@@ -111,13 +130,16 @@ const restaurant = {
 };
 
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-for (const item of menu) console.log(item);
 
-for (const [i,el] of menu.entries()){
-  console.log(`${i+1}: ${el}`);
-}
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const [i,el] of menu.entries()){
+//   console.log(`${i+1}: ${el}`);
+// }
 
 // const rest1={
 //   name: "Capri",
