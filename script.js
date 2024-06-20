d1145c9d97071c48197ab3f238b501b5a5703e62
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-06-20 11:38:44 -0400

    maps set get

--- script.js
+++ script.js
@@ -64,34 +64,27 @@ const restaurant = {
   }
 };
 
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
 
-console.log(ordersSet);
-console.log(newSet('Jonas'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open',11)
+  .set('close',23)
+  .set(true, 'We are open:D')
+  .set(false, 'We are close :(');
 
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
 
-ordersSet.delete('Risotto');
-ordersSet.clear(ordersSet);
-console.log(ordersSet);
+const time =21;
+console.log(rest.get(time>rest.get('open')&& time < rest.get('close')));
 
-for (const order of ordersSet) console.log(order); 
-
-const staff = ["Waiter", 'Chef', 'Waiter', 'Manager','Chef', 'Waiter'];
-
-const staffUnique=[...new Set(staff)];
-
-console.log(staffUnique);
-
-console.log(new Set["Waiter", 'Chef', 'Waiter', 'Manager','Chef', 'Waiter'].size)
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+rest.clear();
