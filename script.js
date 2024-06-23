f46871d29fd3bd75930c5c2b50b84a2770f0d1d3
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-06-22 20:03:42 -0400

    Maps

--- script.js
+++ script.js
@@ -64,27 +64,29 @@ const restaurant = {
   }
 };
 
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
+const question = new Map([
+  ["question", 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2,'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open',11)
-  .set('close',23)
-  .set(true, 'We are open:D')
-  .set(false, 'We are close :(');
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(hoursMap);
 
-const time =21;
-console.log(rest.get(time>rest.get('open')&& time < rest.get('close')));
+for (const [key,value] of question){
+  if (typeof key ==='number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+const answer =3;
+console.log(answer);
+console.log(question.get(question.get('correct')===answer));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-console.log(rest);
-console.log(rest.size);
-rest.clear();
+console.log([...question]);
+console.log(question.entries());
